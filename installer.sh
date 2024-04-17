a60aef337b4748411dd2f3274bdd3c5fbf4741d5
dominikk <dominik.kras@redocean.pl>
2024-04-17 22:38:14 +0200

    add icons and fix config

--- installer.sh
+++ installer.sh
@@ -21,4 +21,60 @@ mkdir -p ~/.config/lf
 mkdir -p ~/.config/nvim
 
 curl -o ~/.config/lf/lfrc https://raw.githubusercontent.com/minikxl/editors-quick-setup/main/lfrc
+curl -o ~/.config/lf/icons https://raw.githubusercontent.com/minikxl/editors-quick-setup/main/lf-icons
 curl -o ~/.config/nvim/init.vim https://raw.githubusercontent.com/minikxl/editors-quick-setup/main/nvimrc
+
+# Wybór powłoki
+echo "Wybierz powłokę:"
+options=("Bash" "Zsh" "Fish")
+select opt in "${options[@]}"; do
+    case $opt in
+        "Bash")
+            shell_config=~/.bashrc
+            break
+            ;;
+        "Zsh")
+            shell_config=~/.zshrc
+            break
+            ;;
+        "Fish")
+            shell_config=~/.config/fish/config.fish
+            break
+            ;;
+        *)
+            echo "Niepoprawna opcja, wybieram Bash jako domyślną powłokę"
+            shell_config=~/.bashrc
+            break
+            ;;
+    esac
+done
+
+# Wybór edytora
+echo "Wybierz edytor domyślny:"
+options=("Neovim" "Vim" "Nano")
+select opt in "${options[@]}"; do
+    case $opt in
+        "Neovim")
+            editor=nvim
+            break
+            ;;
+        "Vim")
+            editor=vim
+            break
+            ;;
+        "Nano")
+            editor=nano
+            break
+            ;;
+        *)
+            echo "Niepoprawna opcja, wybieram Neovim jako domyślny edytor"
+            editor=nvim
+            break
+            ;;
+    esac
+done
+
+echo "source ~/.config/lf/icons" >> $shell_config
+echo "export LF_ICONS" >> $shell_config
+echo "export EDITOR=$editor" >> $shell_config
+echo "export VISUAL=$editor" >> $shell_config
